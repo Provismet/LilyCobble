420a3d98ae0f88443be64e8cf9387901fa5d17d2
Provismet <17149901+Provismet@users.noreply.github.com>
2026-01-07 21:20:47 +0000

    Add more constructors for BattleStatePacketS2C

--- src/main/java/com/provismet/cobblemon/lilycobble/networking/battle/BattleStatePacketS2C.java
+++ src/main/java/com/provismet/cobblemon/lilycobble/networking/battle/BattleStatePacketS2C.java
@@ -2,23 +2,30 @@ package com.provismet.cobblemon.lilycobble.networking.battle;
 
 import com.cobblemon.mod.common.api.battles.interpreter.BattleContext;
 import com.cobblemon.mod.common.api.battles.model.PokemonBattle;
+import com.cobblemon.mod.common.battles.BattleSide;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.provismet.cobblemon.lilycobble.LilyCobbleMain;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.network.packet.s2c.common.CustomPayloadS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
 @SuppressWarnings("unused")
-public record BattleStatePacketS2C(List<String> fieldEffects, List<BattleSideState> sides) implements CustomPayload {
+public record BattleStatePacketS2C (List<String> fieldEffects, List<BattleSideState> sides) implements CustomPayload {
     public static final Id<BattleStatePacketS2C> ID = new Id<>(LilyCobbleMain.identifier("battle_state"));
+    public static final BattleStatePacketS2C NULL_PACKET = new BattleStatePacketS2C(List.of(), List.of());
 
     public static final Codec<BattleStatePacketS2C> CODEC = RecordCodecBuilder.create(instance -> instance.group(
         Codec.STRING.listOf().optionalFieldOf("field_effects", List.of()).forGetter(BattleStatePacketS2C::fieldEffects),
@@ -33,15 +40,40 @@ public record BattleStatePacketS2C(List<String> fieldEffects, List<BattleSideSta
         BattleStatePacketS2C::new
     );
 
+    public BattleStatePacketS2C (Set<String> fieldEffects, List<BattleSideState> sides) {
+        this(fieldEffects.stream().toList(), sides);
+    }
+
+    public BattleStatePacketS2C (Collection<String> fieldEffects, BattleSideState... sides) {
+        this(fieldEffects.stream().toList(), Arrays.stream(sides).toList());
+    }
+
+    public static BattleStatePacketS2C of (BattleSide... sides) {
+        return of(Arrays.stream(sides).toList());
+    }
+
+    public static BattleStatePacketS2C of (List<BattleSide> sides) {
+        if (sides.isEmpty()) return NULL_PACKET;
+
+        Set<String> fieldEffects = getFieldEffects(sides.getFirst().getBattle());
+        List<BattleSideState> sideStates = sides.stream().map(BattleSideState::of).toList();
+        return new BattleStatePacketS2C(fieldEffects, sideStates);
+    }
+
     public static BattleStatePacketS2C of (PokemonBattle battle) {
+        return new BattleStatePacketS2C(
+            getFieldEffects(battle),
+            BattleSideState.of(battle.getSide1()),
+            BattleSideState.of(battle.getSide2())
+        );
+    }
+
+    public static Set<String> getFieldEffects (PokemonBattle battle) {
         Set<String> fieldEffects = new HashSet<>();
         extractContext(fieldEffects, battle, BattleContext.Type.WEATHER);
         extractContext(fieldEffects, battle, BattleContext.Type.ROOM);
         extractContext(fieldEffects, battle, BattleContext.Type.TERRAIN);
-
-        List<BattleSideState> sides = List.of(BattleSideState.of(battle.getSide1()), BattleSideState.of(battle.getSide2()));
-
-        return new BattleStatePacketS2C(fieldEffects.stream().toList(), sides);
+        return fieldEffects;
     }
 
     public Optional<BattleSideState> getSide1 () {
