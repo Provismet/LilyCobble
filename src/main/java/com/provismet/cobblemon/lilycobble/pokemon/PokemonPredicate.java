6af9b73b7404c6b8a49555ac07a4ee4d0d6c999b
Provismet <17149901+Provismet@users.noreply.github.com>
2026-08-13 17:55:05 +0100

    Split PokemonPredicate into two codecs
    Old codec is kept for compatibility

--- src/main/java/com/provismet/cobblemon/lilycobble/pokemon/PokemonPredicate.java
+++ src/main/java/com/provismet/cobblemon/lilycobble/pokemon/PokemonPredicate.java
@@ -7,13 +7,9 @@ import com.provismet.cobblemon.lilycobble.pokemon.matcher.IntPredicate;
 import com.provismet.cobblemon.lilycobble.pokemon.matcher.HeldItemPredicate;
 import com.provismet.cobblemon.lilycobble.pokemon.matcher.StatsPredicate;
 import com.provismet.cobblemon.lilycobble.pokemon.matcher.StringPredicate;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Locale;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
@@ -22,44 +18,41 @@ import java.util.function.Supplier;
  */
 @SuppressWarnings("unused")
 public record PokemonPredicate (
-    StringPredicate speciesShowdownIds,
-    StringPredicate formShowdownIds,
-    StringPredicate speciesFormShowdownIds,
-    StringPredicate aspects,
-    StringPredicate moves,
-    StringPredicate ability,
-    IntPredicate level,
-    IntPredicate friendship,
-    IntPredicate fullness,
-    StringPredicate speciesLabels,
-    StringPredicate formLabels,
-    StatsPredicate EVs,
-    StatsPredicate IVs,
-    HeldItemPredicate heldItem,
-    Optional<Boolean> hasEvolution,
-    Optional<Boolean> hasPreEvolution
+    PokemonSpeciesPredicate species,
+    PokemonInstancePredicate instance
 ) implements Predicate<Pokemon> {
-    public static final Codec<PokemonPredicate> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-        StringPredicate.CODEC.optionalFieldOf("species_showdown_ids", StringPredicate.TRUE).forGetter(PokemonPredicate::speciesShowdownIds),
-        StringPredicate.CODEC.optionalFieldOf("form_showdown_ids", StringPredicate.TRUE).forGetter(PokemonPredicate::formShowdownIds),
-        StringPredicate.CODEC.optionalFieldOf("speciesform_showdown_ids", StringPredicate.TRUE).forGetter(PokemonPredicate::speciesFormShowdownIds),
-        StringPredicate.CODEC.optionalFieldOf("aspects", StringPredicate.TRUE).forGetter(PokemonPredicate::aspects),
-        StringPredicate.CODEC.optionalFieldOf("moves", StringPredicate.TRUE).forGetter(PokemonPredicate::moves),
-        StringPredicate.CODEC.optionalFieldOf("ability", StringPredicate.TRUE).forGetter(PokemonPredicate::ability),
-        IntPredicate.CODEC.optionalFieldOf("level", IntPredicate.TRUE).forGetter(PokemonPredicate::level),
-        IntPredicate.CODEC.optionalFieldOf("friendship", IntPredicate.TRUE).forGetter(PokemonPredicate::friendship),
-        IntPredicate.CODEC.optionalFieldOf("fullness", IntPredicate.TRUE).forGetter(PokemonPredicate::fullness),
-        StringPredicate.CODEC.optionalFieldOf("species_labels", StringPredicate.TRUE).forGetter(PokemonPredicate::speciesLabels),
-        StringPredicate.CODEC.optionalFieldOf("form_labels", StringPredicate.TRUE).forGetter(PokemonPredicate::formLabels),
-        StatsPredicate.CODEC.optionalFieldOf("EVs", StatsPredicate.TRUE).forGetter(PokemonPredicate::EVs),
-        StatsPredicate.CODEC.optionalFieldOf("IVs", StatsPredicate.TRUE).forGetter(PokemonPredicate::IVs),
-        HeldItemPredicate.CODEC.optionalFieldOf("held_item", HeldItemPredicate.TRUE).forGetter(PokemonPredicate::heldItem),
-        Codec.BOOL.optionalFieldOf("has_evolution").forGetter(PokemonPredicate::hasEvolution),
-        Codec.BOOL.optionalFieldOf("has_pre-evolution").forGetter(PokemonPredicate::hasPreEvolution)
+    private static final Codec<PokemonPredicate> CODEC_NEW = RecordCodecBuilder.create(instance -> instance.group(
+        PokemonSpeciesPredicate.CODEC.optionalFieldOf("species", PokemonSpeciesPredicate.TRUE).forGetter(PokemonPredicate::species),
+        PokemonInstancePredicate.CODEC.optionalFieldOf("instance", PokemonInstancePredicate.TRUE).forGetter(PokemonPredicate::instance)
     ).apply(instance, PokemonPredicate::new));
 
-    public static final PokemonPredicate TRUE = new PokemonPredicate(StringPredicate.TRUE, StringPredicate.TRUE, StringPredicate.TRUE, StringPredicate.TRUE, StringPredicate.TRUE, StringPredicate.TRUE, IntPredicate.TRUE, IntPredicate.TRUE, IntPredicate.TRUE, StringPredicate.TRUE, StringPredicate.TRUE, StatsPredicate.TRUE, StatsPredicate.TRUE, HeldItemPredicate.TRUE, Optional.empty(), Optional.empty());
-    public static final PokemonPredicate FALSE = new PokemonPredicate(StringPredicate.FALSE, StringPredicate.FALSE, StringPredicate.FALSE, StringPredicate.FALSE, StringPredicate.FALSE, StringPredicate.FALSE, IntPredicate.FALSE, IntPredicate.FALSE, IntPredicate.FALSE, StringPredicate.FALSE, StringPredicate.FALSE, StatsPredicate.FALSE, StatsPredicate.FALSE, HeldItemPredicate.FALSE, Optional.empty(), Optional.empty());
+    private static final Codec<PokemonPredicate> CODEC_OLD = RecordCodecBuilder.create(instance -> instance.group(
+        StringPredicate.CODEC.optionalFieldOf("species_showdown_ids", StringPredicate.TRUE).forGetter(p -> p.species.speciesShowdownIds()),
+        StringPredicate.CODEC.optionalFieldOf("form_showdown_ids", StringPredicate.TRUE).forGetter(p -> p.species.formShowdownIds()),
+        StringPredicate.CODEC.optionalFieldOf("speciesform_showdown_ids", StringPredicate.TRUE).forGetter(p -> p.species.speciesFormShowdownIds()),
+        StringPredicate.CODEC.optionalFieldOf("aspects", StringPredicate.TRUE).forGetter(p -> p.instance.aspects()),
+        StringPredicate.CODEC.optionalFieldOf("moves", StringPredicate.TRUE).forGetter(p -> p.instance.moves()),
+        StringPredicate.CODEC.optionalFieldOf("ability", StringPredicate.TRUE).forGetter(p -> p.instance.ability()),
+        IntPredicate.CODEC.optionalFieldOf("level", IntPredicate.TRUE).forGetter(p -> p.instance.level()),
+        IntPredicate.CODEC.optionalFieldOf("friendship", IntPredicate.TRUE).forGetter(p -> p.instance.friendship()),
+        IntPredicate.CODEC.optionalFieldOf("fullness", IntPredicate.TRUE).forGetter(p -> p.instance.fullness()),
+        StringPredicate.CODEC.optionalFieldOf("species_labels", StringPredicate.TRUE).forGetter(p -> p.species.speciesLabels()),
+        StringPredicate.CODEC.optionalFieldOf("form_labels", StringPredicate.TRUE).forGetter(p -> p.species.formLabels()),
+        StatsPredicate.CODEC.optionalFieldOf("EVs", StatsPredicate.TRUE).forGetter(p -> p.instance.EVs()),
+        StatsPredicate.CODEC.optionalFieldOf("IVs", StatsPredicate.TRUE).forGetter(p -> p.instance.IVs()),
+        HeldItemPredicate.CODEC.optionalFieldOf("held_item", HeldItemPredicate.TRUE).forGetter(p -> p.instance.heldItem()),
+        Codec.BOOL.optionalFieldOf("has_evolution").forGetter(p -> p.species.hasEvolution()),
+        Codec.BOOL.optionalFieldOf("has_pre-evolution").forGetter(p -> p.species.hasPreEvolution())
+    ).apply(instance, (speciesId, formId, speciesFormId, aspects, moves, abilty, level, friendship, fullness, speciesLabel, formLabel, ev, iv, held, hasEvo, hasPreEvo) ->
+        new PokemonPredicate(
+            new PokemonSpeciesPredicate(speciesId, formId, speciesFormId, speciesLabel, formLabel, StringPredicate.TRUE, StatsPredicate.TRUE, hasEvo, hasPreEvo),
+            new PokemonInstancePredicate(aspects, moves, abilty, StringPredicate.TRUE, level, friendship, fullness, ev, iv, held))
+    ));
+
+    public static final Codec<PokemonPredicate> CODEC = Codec.withAlternative(CODEC_NEW, CODEC_OLD);
+
+    public static final PokemonPredicate TRUE = new PokemonPredicate(PokemonSpeciesPredicate.TRUE, PokemonInstancePredicate.TRUE);
+    public static final PokemonPredicate FALSE = new PokemonPredicate(PokemonSpeciesPredicate.FALSE, PokemonInstancePredicate.FALSE);
 
     public static Builder builder () {
         return new Builder();
@@ -68,236 +61,39 @@ public record PokemonPredicate (
     public boolean test (@Nullable Pokemon pokemon) {
         if (pokemon == null) return this.equals(TRUE);
 
-        return this.speciesShowdownIds.test(pokemon.getSpecies().showdownId())
-            && this.formShowdownIds.test(pokemon.getForm().formOnlyShowdownId())
-            && this.speciesFormShowdownIds.test(pokemon.showdownId())
-            && this.aspects.test(pokemon.getAspects())
-            && this.moves.test(pokemon.getMoveSet().getMoves().stream().filter(Objects::nonNull).map(move -> move.getName().toLowerCase(Locale.ROOT)).toList())
-            && this.ability.test(pokemon.getAbility().getTemplate().getName())
-            && this.level.test(pokemon.getLevel())
-            && this.friendship.test(pokemon.getFriendship())
-            && this.fullness.test(pokemon.getCurrentFullness())
-            && this.speciesLabels.test(pokemon.getSpecies().getLabels())
-            && this.formLabels.test(pokemon.getForm().getLabels())
-            && this.EVs.test(pokemon.getEvs())
-            && this.IVs.test(pokemon.getIvs())
-            && this.heldItem.test(pokemon.getHeldItem$common())
-            && (this.hasEvolution.isEmpty() || this.hasEvolution().get() == !pokemon.getForm().getEvolutions().isEmpty())
-            && (this.hasPreEvolution.isEmpty() || this.hasPreEvolution.get() == (pokemon.getPreEvolution() != null));
+        return this.species.test(pokemon)
+            && this.instance.test(pokemon);
     }
 
     public static class Builder implements Supplier<PokemonPredicate> {
-        private StringPredicate speciesId = StringPredicate.TRUE;
-        private StringPredicate formId = StringPredicate.TRUE;
-        private StringPredicate speciesFormId = StringPredicate.TRUE;
-        private StringPredicate aspects = StringPredicate.TRUE;
-        private StringPredicate moves = StringPredicate.TRUE;
-        private StringPredicate ability = StringPredicate.TRUE;
-        private IntPredicate level = IntPredicate.TRUE;
-        private IntPredicate friendship = IntPredicate.TRUE;
-        private IntPredicate fullness = IntPredicate.TRUE;
-        private StringPredicate speciesLabels = StringPredicate.TRUE;
-        private StringPredicate formLabels = StringPredicate.TRUE;
-        private StatsPredicate EVs = StatsPredicate.TRUE;
-        private StatsPredicate IVs = StatsPredicate.TRUE;
-        private HeldItemPredicate heldItem = HeldItemPredicate.TRUE;
-        private Boolean hasEvolution = null;
-        private Boolean hasPreEvolution = null;
+        PokemonSpeciesPredicate species = PokemonSpeciesPredicate.TRUE;
+        PokemonInstancePredicate instance = PokemonInstancePredicate.TRUE;
 
-        public Builder species (StringPredicate species) {
-            this.speciesId = species;
+        public Builder species (PokemonSpeciesPredicate species) {
+            this.species = species;
             return this;
         }
 
-        public Builder species (StringPredicate.Builder builder) {
-            this.speciesId = builder.build();
+        public Builder species (PokemonSpeciesPredicate.Builder builder) {
+            this.species = builder.build();
             return this;
         }
 
-        public Builder species (String species) {
-            this.speciesId = StringPredicate.builder().whitelist(species).build();
+        public Builder instance (PokemonInstancePredicate instance) {
+            this.instance = instance;
             return this;
         }
 
-        public Builder form (StringPredicate form) {
-            this.formId = form;
-            return this;
-        }
-
-        public Builder form (StringPredicate.Builder builder) {
-            this.formId = builder.build();
-            return this;
-        }
-
-        public Builder form (String form) {
-            this.formId = StringPredicate.builder().whitelist(form).build();
-            return this;
-        }
-
-        public Builder speciesForm (StringPredicate speciesForm) {
-            this.speciesFormId = speciesForm;
-            return this;
-        }
-
-        public Builder speciesForm (StringPredicate.Builder builder) {
-            this.speciesFormId = builder.build();
-            return this;
-        }
-
-        public Builder speciesForm (String speciesForm) {
-            this.speciesFormId = StringPredicate.builder().whitelist(speciesForm).build();
-            return this;
-        }
-
-        public Builder aspects (StringPredicate aspects) {
-            this.aspects = aspects;
-            return this;
-        }
-
-        public Builder aspects (StringPredicate.Builder builder) {
-            this.aspects = builder.build();
-            return this;
-        }
-
-        public Builder moves (StringPredicate moves) {
-            this.moves = moves;
-            return this;
-        }
-
-        public Builder moves (StringPredicate.Builder builder) {
-            this.moves = builder.build();
-            return this;
-        }
-
-        public Builder ability (StringPredicate ability) {
-            this.ability = ability;
-            return this;
-        }
-
-        public Builder ability (StringPredicate.Builder ability) {
-            this.ability = ability.build();
-            return this;
-        }
-
-        public Builder ability (String ability) {
-            this.ability = StringPredicate.builder().whitelist(ability).build();
-            return this;
-        }
-
-        public Builder speciesLabels (StringPredicate speciesLabels) {
-            this.speciesLabels = speciesLabels;
-            return this;
-        }
-
-        public Builder speciesLabels (StringPredicate.Builder builder) {
-            this.speciesLabels = builder.build();
-            return this;
-        }
-
-        public Builder speciesLabels (String speciesLabel) {
-            this.speciesLabels = StringPredicate.builder().whitelist(speciesLabel).build();
-            return this;
-        }
-
-        public Builder formLabels (StringPredicate formLabels) {
-            this.formLabels = formLabels;
-            return this;
-        }
-
-        public Builder formLabels (StringPredicate.Builder builder) {
-            this.formLabels = builder.build();
-            return this;
-        }
-
-        public Builder level (IntPredicate matcher) {
-            this.level = matcher;
-            return this;
-        }
-
-        public Builder friendship (IntPredicate matcher) {
-            this.friendship = matcher;
-            return this;
-        }
-
-        public Builder fullness (IntPredicate matcher) {
-            this.fullness = matcher;
-            return this;
-        }
-
-        public Builder EVs (StatsPredicate matcher) {
-            this.EVs = matcher;
-            return this;
-        }
-
-        public Builder EVs (StatsPredicate.Builder matcher) {
-            this.EVs = matcher.build();
-            return this;
-        }
-
-        public Builder IVs (StatsPredicate matcher) {
-            this.IVs = matcher;
-            return this;
-        }
-
-        public Builder IVs (StatsPredicate.Builder matcher) {
-            this.IVs = matcher.build();
-            return this;
-        }
-
-        public Builder heldItem (HeldItemPredicate heldItem) {
-            this.heldItem = heldItem;
-            return this;
-        }
-
-        public Builder heldItem (HeldItemPredicate.Builder builder) {
-            this.heldItem = builder.build();
-            return this;
-        }
-
-        public Builder heldItem (String showdownId) {
-            this.heldItem = HeldItemPredicate.builder().whitelist(showdownId).build();
-            return this;
-        }
-
-        public Builder heldItem (Item item) {
-            this.heldItem = HeldItemPredicate.builder().whitelist(item.getDefaultStack()).build();
-            return this;
-        }
-
-        public Builder heldItem (ItemStack item) {
-            this.heldItem = HeldItemPredicate.builder().whitelist(item).build();
-            return this;
-        }
-
-        public Builder hasEvolution (boolean hasEvolution) {
-            this.hasEvolution = hasEvolution;
-            return this;
-        }
-
-        public Builder hasPreEvolution (boolean hasPreEvolution) {
-            this.hasPreEvolution = hasPreEvolution;
+        public Builder instance (PokemonInstancePredicate.Builder builder) {
+            this.instance = builder.build();
             return this;
         }
 
         @Override
         public PokemonPredicate get () {
             return new PokemonPredicate(
-                Objects.requireNonNull(this.speciesId),
-                Objects.requireNonNull(this.formId),
-                Objects.requireNonNull(this.speciesFormId),
-                Objects.requireNonNull(this.aspects),
-                Objects.requireNonNull(this.moves),
-                Objects.requireNonNull(this.ability),
-                Objects.requireNonNull(this.level),
-                Objects.requireNonNull(this.friendship),
-                Objects.requireNonNull(this.fullness),
-                Objects.requireNonNull(this.speciesLabels),
-                Objects.requireNonNull(this.formLabels),
-                Objects.requireNonNull(this.EVs),
-                Objects.requireNonNull(this.IVs),
-                Objects.requireNonNull(this.heldItem),
-                Optional.ofNullable(this.hasEvolution),
-                Optional.ofNullable(this.hasPreEvolution)
+                Objects.requireNonNull(this.species),
+                Objects.requireNonNull(this.instance)
             );
         }
     }
